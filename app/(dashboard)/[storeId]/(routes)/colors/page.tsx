abc27dbe3388d3bd808db3f974864bd8e42137d2
Tran Dieu <tracpham2210@gmail.com>
2025-12-25 23:31:02 +0700

    complete ui/ux,logic size and color

---
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,35 @@
+import prismadb from "@/lib/prismadb";
+import { ColorClient } from "./components/client";
+import { ColorColumn } from "./components/columns";
+import { formatDate } from "@/utils/format";
+
+const ColorsPage = async ({
+   params,
+}: {
+   params: Promise<{ storeId: string }>;
+}) => {
+   const { storeId } = await params;
+
+   const colors = await prismadb.color.findMany({
+      where: {
+         storeId,
+      },
+      orderBy: {
+         createdAt: 'desc'
+
+      }
+   });
+
+   const formattedColors: ColorColumn[] = colors.map((color) => ({
+      id: color.id,
+      name: color.name,
+      value: color.value,
+      createdAt: formatDate(color.createdAt),
+   }))
+   return <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+         <ColorClient data={formattedColors} />
+      </div>
+   </div>;
+}
+export default ColorsPage;
---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,35 @@
+import prismadb from "@/lib/prismadb";
+import { SizeClient } from "./components/client";
+import { SizeColumn } from "./components/columns";
+import { formatDate } from "@/utils/format";
+
+const SizesPage = async ({
+   params,
+}: {
+   params: Promise<{ storeId: string }>;
+}) => {
+   const { storeId } = await params;
+
+   const sizes = await prismadb.size.findMany({
+      where: {
+         storeId,
+      },
+      orderBy: {
+         createdAt: 'desc'
+
+      }
+   });
+
+   const formattedSizes: SizeColumn[] = sizes.map((size) => ({
+      id: size.id,
+      name: size.name,
+      value: size.value,
+      createdAt: formatDate(size.createdAt),
+   }))
+   return <div className="flex flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+         <SizeClient data={formattedSizes} />
+      </div>
+   </div>;
+}
+export default SizesPage;
